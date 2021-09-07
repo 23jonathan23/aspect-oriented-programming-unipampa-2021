047a635c138d65433ff53066d16c99ee6d6b803c
Jonathan Raphael <jonathanraphael74@outlook.com>
2021-09-07 11:19:46 -0300

    spring boot setup

---
+++ Entrega 4/sales-online-games/src/main/java/com/sales/online/games/salesonlinegames/SalesOnlineGamesApplication.java
@@ -0,0 +1,13 @@
+package com.sales.online.games.salesonlinegames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesOnlineGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesOnlineGamesApplication.class, args);
+	}
+
+}
---
+++ Entrega 4/sales-online-games/src/test/java/com/sales/online/games/salesonlinegames/SalesOnlineGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sales.online.games.salesonlinegames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesOnlineGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
