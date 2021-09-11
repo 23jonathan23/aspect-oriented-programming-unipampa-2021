456bb9f5e66697468e135aced84c70f8db72b62b
2010100589 <gp.matheusluis@gmail.com>
2021-09-11 09:52:12 -0300

    added coment to future feature

--- Entrega 4/sales-online-games/src/main/java/com/sales/online/games/salesonlinegames/Presentation/Startup.java
+++ Entrega 4/sales-online-games/src/main/java/com/sales/online/games/salesonlinegames/Presentation/Startup.java
@@ -2,8 +2,10 @@ package com.sales.online.games.salesonlinegames.Presentation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+//@ComponentScan(basePackages = "com.sales.online.games.salesonlinegames")
 public class Startup {
 	public static void main(String[] args) {
 		SpringApplication.run(Startup.class, args);
